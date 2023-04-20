401a84942bbc0ef8ed3abb28a76c03f5016cb3a1
Wodson Correia <wodsonluiz@live.com>
2023-04-20 17:47:15 -0300

    Add redlock

---
+++ src/Worker.Core.WithRedLock/Program.cs
@@ -0,0 +1,40 @@
+﻿using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using System.Net;
+
+namespace Worker.Core.WithRedLock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var redlockEndPoints = new List<RedLockEndPoint>
+            {
+                new RedLockEndPoint
+                {
+                    EndPoints =
+                    {
+                        new DnsEndPoint("localhost", 6376)
+                    },
+                    ConnectionTimeout = 5000,
+                    Password = "admin123",
+                    RedisDatabase = 2,
+                    
+                },
+                new RedLockEndPoint
+                {
+                    EndPoint = new DnsEndPoint("localhost", 6377),
+                    ConnectionTimeout = 5000,
+                    Password = "admin123",
+                    RedisDatabase = 2,
+                }
+            };
+
+            using (var redlockFactory = RedLockFactory.Create(redlockEndPoints))
+            {
+                var ttl = TimeSpan.FromSeconds(400);
+                var redLock = redlockFactory.CreateLockAsync("lab:teste:redis", ttl).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
